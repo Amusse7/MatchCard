b8fc06fa4173b8d7c4bb427b3c10c697daa09b46
Abdulkadir Musse <72959571+Amusse7@users.noreply.github.com>
2023-11-16 03:58:47 -0500

    Frist commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import random
+
+# Create a list of cards
+cards = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'] * 2
+random.shuffle(cards)
+
+# Function to display the current board
+def display_board(board):
+    for row in board:
+        print(" ".join(row))
+
+# Function to initialize the board
+def initialize_board():
+    board = []
+    for _ in range(4):
+        row = ['*'] * 4
+        board.append(row)
+    return board
+
+# Function to play the game
+def play_game():
+    board = initialize_board()
+    display_board(board)
+    matched = set()
+    attempts = 0
+
+    while len(matched) < len(cards):
+        try:
+            print("\nEnter the row and column number (e.g., '1 2'):")
+            row1, col1 = map(int, input("Enter position of the first card: ").split())
+            row2, col2 = map(int, input("Enter position of the second card: ").split())
+
+            if (row1 == row2 and col1 == col2) or board[row1 - 1][col1 - 1] != '*' or board[row2 - 1][col2 - 1] != '*':
+                print("Invalid input or cards already matched! Try again.")
+                continue
+
+            if cards[(row1 - 1) * 4 + (col1 - 1)] == cards[(row2 - 1) * 4 + (col2 - 1)]:
+                board[row1 - 1][col1 - 1] = cards[(row1 - 1) * 4 + (col1 - 1)]
+                board[row2 - 1][col2 - 1] = cards[(row2 - 1) * 4 + (col2 - 1)]
+                matched.add(cards[(row1 - 1) * 4 + (col1 - 1)])
+                print("Match found!")
+            else:
+                print("No match! Try again.")
+            
+            display_board(board)
+            attempts += 1
+        except (ValueError, IndexError):
+            print("Invalid input! Please enter valid row and column numbers.")
+
+    print(f"Congratulations! You've matched all cards in {attempts} attempts.")
+
+# Start the game
+play_game()
